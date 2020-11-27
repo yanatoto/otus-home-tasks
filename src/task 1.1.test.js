193a33e8058f491d6235dc9c73d759fcaec3af8d
Yana <yanas@bluewin.ch>
2020-11-27 15:34:43 +0100

    Configure jest with babel

---
+++ src/task 1.1.test.js
@@ -0,0 +1,16 @@
+import { sum } from "./task 1.1";
+
+describe("sum", () => {
+  it("returns sum of params", () => {
+    expect(sum(1, 2)).toBe(3);
+  }); 
+});
+
+import { mult } from "./task 1.1";
+
+describe("mult", () => {
+    
+    it("returns mult of params", () => {
+      expect(mult(1, 2)).toBe(2);
+    });  
+  });
