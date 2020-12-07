3056283b37c5e8d71fd185ca2304747474372c97
Yana <yanas@bluewin.ch>
2020-12-07 14:59:24 +0100

    Fix task1.2.test.js

--- src/task1.2.test.js
+++ src/task1.2.test.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 import { getStrLength } from "./task1.2";
 
-describe("sum", () => {
+describe("getStrLength", () => {
   it("returns sum of lengths", () => {
-    expect(getStrLength("yana", "salvatore")).toBe(console.log(13));
+    expect(getStrLength("yana", "salvatore")).toBe(13);
   });
 });
