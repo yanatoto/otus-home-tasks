ad6e654c9c97e7edbe54bdd92005a47e14569131
Yana <yanas@bluewin.ch>
2020-12-10 11:12:55 +0100

    Fix task1.3.test.js

--- src/task1.3.test.js
+++ src/task1.3.test.js
@@ -6,7 +6,7 @@ describe("consoleSumOfPromptDigits", () => {
   it("logs to console sum of prompt digits", () => {
     jest.spyOn(window, "prompt").mockImplementation(() => "123");
     jest.spyOn(console, "log");
-    sumOfPromptDigits();
-    expect(console.log).toHaveBeenCalledWith("6");
+
+    expect(sumOfPromptDigits()).toBe(6);
   });
 });
