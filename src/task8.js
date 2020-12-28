3fad2e5fc3d599b602eab01b7dee601211f9d9c3
Yana <yanas@bluewin.ch>
2020-12-28 17:31:43 +0100

    Add task8.3

--- src/task8.js
+++ src/task8.js
@@ -33,3 +33,26 @@ export function getMinsPassed() {
 
   return minsPass;
 }
+
+// 3.*В двух переменных хранятся даты рождения двух
+// пользователей в формате ДД.ММ.ГГГГ. Написать
+// программу, которая определяет более молодого
+// пользователя.
+
+export function getYoungerUser() {
+  const user1 = "01.10.1995";
+  const [day1, month1, year1] = user1.split(".");
+  const date1 = new Date(`${year1}-${month1}-${day1}`);
+
+  const user2 = "01.05.1992";
+  const [day2, month2, year2] = user2.split(".");
+  const date2 = new Date(`${year2}-${month2}-${day2}`);
+
+  const user1Age = Date.now() - date1;
+  const user2Age = Date.now() - date2;
+
+  if (user1Age < user2Age) {
+    return console.log("user1 is younger");
+  }
+  return console.log("user2 is younger");
+}
