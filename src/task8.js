64fb05cf087f2b6b32ed64b944eeae7e629babf8
Yana <yanas@bluewin.ch>
2020-12-29 16:27:42 +0100

    Fix task8 and add task8.test

--- src/task8.js
+++ src/task8.js
@@ -39,20 +39,12 @@ export function getMinsPassed() {
 // программу, которая определяет более молодого
 // пользователя.
 
-export function getYoungerUser() {
-  const user1 = "01.10.1995";
-  const [day1, month1, year1] = user1.split(".");
-  const date1 = new Date(`${year1}-${month1}-${day1}`);
+export function getYoungerUser(user1, user2) {
+  const date1 = new Date(user1);
+  const date2 = new Date(user2);
 
-  const user2 = "01.05.1992";
-  const [day2, month2, year2] = user2.split(".");
-  const date2 = new Date(`${year2}-${month2}-${day2}`);
-
-  const user1Age = Date.now() - date1;
-  const user2Age = Date.now() - date2;
-
-  if (user1Age < user2Age) {
-    return console.log("user1 is younger");
+  if (date1.getTime() > date2.getTime()) {
+    return date1;
   }
-  return console.log("user2 is younger");
+  return date2;
 }
--- src/task8.test.js
+++ src/task8.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
-import { getWeekDay, getMinsPassed } from "./task8";
+import { getWeekDay, getMinsPassed, getYoungerUser } from "./task8";
 
-describe("getWeekDay, getMinsPassed", () => {
+describe("getWeekDay, getMinsPassed, getYoungerUser", () => {
   test("Returns the day of the week", () => {
     jest.spyOn(global.window, "prompt").mockImplementation(() => "01.12.2020");
     expect(getWeekDay()).toEqual("Tuesday");
@@ -11,5 +11,11 @@ describe("getWeekDay, getMinsPassed", () => {
     const now = new Date();
     const res = now.getHours() * 60 + now.getMinutes();
     expect(getMinsPassed()).toBe(res);
+  });
+
+  test("Returns younger user", () => {
+    const user1 = new Date(1, 10, 1995);
+    const user2 = new Date(1, 5, 1982);
+    expect(getYoungerUser(user1, user2)).toEqual(user1);
   });
 });
