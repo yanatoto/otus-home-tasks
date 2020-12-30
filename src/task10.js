ddb21514dd625c0588a32c444b6bed9c9aed3de8
Yana <yanas@bluewin.ch>
2020-12-30 19:00:58 +0100

    Add task10

---
+++ src/task10.js
@@ -0,0 +1,26 @@
+/* eslint-disable max-len */
+/* eslint-disable no-alert */
+// Пользователь вводит текстовую строку. Определить с
+// помощью регулярного выражения, является ли
+// введённая строка:
+// 1.Датой.
+// 2.Адресом электронной почты.
+// 3.*Номером телефона.
+
+export function isItDate() {
+  const regexp = /^\d{2}-\d{2}-\d{4}$/;
+  const date = prompt("Enter the date: ");
+  return regexp.test(date);
+}
+
+export function isItEmail() {
+  const regexp = /^[^0-9.\-_@&$?][0-9.\-_a-zA-Z]+@[a-z0-9]+\.[a-z]{2,6}/;
+  const email = prompt("Enter email: ");
+  return regexp.test(email);
+}
+
+export function isItPhoneNumber() {
+  const regexp = /^\+?[0-9]{1,4}-?[0-9]{7}$|(^\+?[0-9]{1,4}-?[0-9]{3}-?[0-9]{4}$)|(^\+?[0-9]{1,4}-?[0-9]{3}-{1}[0-9]{2}-{1}[0-9]{2}$)/;
+  const phone = prompt("Enter mobile number: ");
+  return regexp.test(phone);
+}
