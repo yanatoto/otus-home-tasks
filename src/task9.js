0b60f765d490f74aec425083f0a7129b41099b7b
Yana <yanas@bluewin.ch>
2020-12-30 14:01:10 +0100

    Add task9.2

--- src/task9.js
+++ src/task9.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-alert */
 /* eslint-disable no-console */
 // 1.Даны длины трёх сторон треугольника. Определить,
 // является ли треугольник прямоугольным.
@@ -9,4 +10,21 @@ export function rightTriangle(a, b, c) {
   return false;
 }
 
-// console.log(rightTriangle(3, 4, 5));
+// 2.Пользователь вводит число R. Написать программу,
+// которая выведет в консоль длину окружности и
+// площадь круга с радиусом R.
+
+export function getPerimeterOfCircle() {
+  const radius = prompt("Enter a radius: ");
+  const perimeter = 2 * Math.PI * radius;
+
+  return perimeter;
+}
+console.log(getPerimeterOfCircle());
+
+export function getSquareOfCircle() {
+  const radius = prompt("Enter a radius: ");
+  const square = Math.PI * radius ** 2;
+
+  return square;
+}
