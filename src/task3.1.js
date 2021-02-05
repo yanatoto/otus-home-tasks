a1b8fff69329fac3cbb4b426d75fcab1c2e490f4
Yana <yanas@bluewin.ch>
2021-02-05 21:03:29 +0100

    Correct task3.1.test.js

--- src/task3.1.js
+++ src/task3.1.js
@@ -4,8 +4,9 @@
 // 100.
 
 export function getNumbers() {
+  let sum = 0;
   for (let i = 50; i <= 100; i++) {
-    console.log(i);
+    sum += i;
   }
+  console.log(sum);
 }
-getNumbers();
--- src/task3.1.test.js
+++ src/task3.1.test.js
@@ -3,6 +3,8 @@ import { getNumbers } from "./task3.1";
 
 describe("getNumbers", () => {
   it("console numbers from 50 till 100", () => {
-    expect(getNumbers()).toBe(console.log(50 - 100));
+    jest.spyOn(console, "log");
+    getNumbers();
+    expect(console.log).toBeCalledWith(3825);
   });
 });
