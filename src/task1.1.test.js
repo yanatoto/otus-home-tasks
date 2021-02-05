35a7cdebb7641c4e9a2d4f0f7fff5f19404c6a6e
Yana <yanas@bluewin.ch>
2021-02-05 19:05:03 +0100

    Change task1.1

--- src/task1.1.test.js
+++ src/task1.1.test.js
@@ -3,12 +3,12 @@ import { sum, mult } from "./task1.1";
 
 describe("sum", () => {
   it("returns sum of params", () => {
-    expect(sum(2, 3)).toBe(console.log(5));
+    expect(sum(2, 3)).toBe(5);
   });
 });
 
 describe("mult", () => {
   it("returns multiplication of params", () => {
-    expect(mult(2, 3)).toBe(console.log(6));
+    expect(mult(2, 3)).toBe(6);
   });
 });
