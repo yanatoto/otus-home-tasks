995f20a7b16606523b8b73697f43b6c37df6d9c4
Yana <yanas@bluewin.ch>
2021-02-06 19:55:26 +0100

    Amend task5.3

--- src/task5.3.js
+++ src/task5.3.js
@@ -2,9 +2,7 @@
 // наименьший элементы исходного массива.
 export function getMinMax() {
   const numbers = [45, 15, 18, 59, 22, 27, 1, 31, 38, 51];
-  numbers.sort(function (a, b) {
-    return a - b;
-  });
+  numbers.sort((a, b) => a - b);
 
   console.log(numbers[0], numbers[9]);
 }
